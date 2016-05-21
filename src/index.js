ff3b2de292e19603c841022885a88ff3e5291998
euglazer <eugenestapler@gmail.com>
2016-05-21 16:07:16 -0700

    can get deleted words

--- src/index.js
+++ src/index.js
@@ -4,8 +4,8 @@ import Diff from 'text-diff'
 var diff = new Diff()
 import filter from 'lodash.filter'
 import Tokenizer from 'sentence-tokenizer'
-var tokenizer = new Tokenizer()
-import flatten from 'lodash.flatten'
+var tokenizer = new Tokenizer('')
+import flattenDeep from 'lodash.flattendeep'
 
 var snapshots = ['']
 var $pad = $('#pad')
@@ -17,12 +17,22 @@ $('#store-version-btn').one('click', function (e) {
 
 $('#store-version-btn').click((e) => {
   var snapshot = $pad.val()
+
   var differences = diff.main(last(snapshots), snapshot)
-  var deletions = flatten(filter(differences, (difference) => {
+
+  var deletions = flattenDeep(filter(differences, (difference) => {
     return difference[0] === -1
   }).map((difference) => {
     return difference[1]
   }))
-  console.log('deletions: ', deletions)
+
+  var deletedWords = flattenDeep(deletions.map((deletion) => {
+    tokenizer.setEntry(deletion)
+    tokenizer.getSentences()
+    return tokenizer.getTokens()
+  }))
+
+  console.log('deletedWords: ', deletedWords)
+
   snapshots.push(snapshot)
 })
