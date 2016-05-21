94d70291801e87645aea0aa9f3a2292a61f346fa
euglazer <eugenestapler@gmail.com>
2016-05-21 15:52:04 -0700

    get all deletions

--- src/index.js
+++ src/index.js
@@ -1,6 +1,13 @@
 import $ from 'jquery'
+import last from 'lodash.last'
+import Diff from 'text-diff'
+var diff = new Diff()
+import filter from 'lodash.filter'
+import Tokenizer from 'sentence-tokenizer'
+var tokenizer = new Tokenizer()
+import flatten from 'lodash.flatten'
 
-var snapshots = []
+var snapshots = ['']
 var $pad = $('#pad')
 
 $('#store-version-btn').one('click', function (e) {
@@ -9,6 +16,13 @@ $('#store-version-btn').one('click', function (e) {
 
 
 $('#store-version-btn').click((e) => {
-  snapshots.push($pad.val())
-  console.log(snapshots)
+  var snapshot = $pad.val()
+  var differences = diff.main(last(snapshots), snapshot)
+  var deletions = flatten(filter(differences, (difference) => {
+    return difference[0] === -1
+  }).map((difference) => {
+    return difference[1]
+  }))
+  console.log('deletions: ', deletions)
+  snapshots.push(snapshot)
 })
